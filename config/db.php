4a785107b472ee6c977d58bf9af0826997e414ac
manuelhs10 <manuelhidalgosalas10@gmail.com>
2025-10-30 10:04:35 +0100

    crear bbdd server

--- config/db.php
+++ config/db.php
@@ -1,1 +1,16 @@
-<?php //config ?>
+<?php
+//prueba
+$host = "localhost";
+$usuario = "root";
+$password = "";
+$base_datos = "practicas_server";
+
+try {
+    $conexion = new PDO("mysql:host=$host;dbname=$base_datos;charset=utf8", $usuario, $password);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión correcta a practicas_server";
+
+} catch (PDOException $e) {
+    echo "Error de conexión: " . $e->getMessage();
+}
+?>
