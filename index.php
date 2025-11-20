60388f798ca26fca67cfe8e236b1d91b6ed780a9
manuelhs10 <manuelhidalgosalas10@gmail.com>
2025-11-20 11:08:35 +0100

    Creación de Index

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Prácticas ALTAIR CSIF</title>
+    <link rel="stylesheet" href="assets/css/index_style.css">
+</head>
+
+<body>
+
+    <div class="contenedor">
+
+        <h1>Prácticas ALTAIR CSIF</h1>
+
+        <div class="botones">
+            <a href="vistas/formulario_empresa.php" class="btn">Dar de Alta</a>
+            <a href="vistas/listado_practicas.php" class="btn">Listado Prácticas</a>
+        </div>
+
+    </div>
+
+</body>
+</html>
 
